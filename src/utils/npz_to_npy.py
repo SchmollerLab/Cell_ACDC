3245b88617a98a01935a38fc6535611471a90d55
Kukhtevich <igor.kukhtevich@scidom.de>
2021-07-27 13:14:51 +0200

    normpath to realpath

--- src/utils/npz_to_npy.py
+++ src/utils/npz_to_npy.py
@@ -2,7 +2,7 @@ import os
 import sys
 import numpy as np
 
-src_path = os.path.dirname(os.path.dirname(os.path.normpath(__file__)))
+src_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append(src_path)
 
 import prompts
