c213226f883fcdf496abcbafba9d729a99d8dd00
Francesco <padovaf@tcd.ie>
2022-02-06 18:49:42 +0100

    Warning that acdc is going to install yeastmate

--- cellacdc/models/YeastMate/__init__.py
+++ cellacdc/models/YeastMate/__init__.py
@@ -2,6 +2,8 @@ import os
 import sys
 import subprocess
 
+from PyQt5.QtWidgets import QMessageBox
+
 try:
     import detectron2
 except ModuleNotFoundError:
@@ -13,6 +15,22 @@ except ModuleNotFoundError:
 try:
     import yeastmatedetector
 except ModuleNotFoundError:
+    msg = QMessageBox()
+    txt = (
+        'Cell-ACDC is going to download and install "YeastMate".\n\n'
+        'Make sure you have an active internet connection, '
+        'before continuing. '
+        'Progress will be displayed on the terminal\n\n'
+        'Alternatively, you can cancel the process and try later.'
+    )
+    answer = msg.information(
+        None, 'Install YeastMate', txt, msg.Ok | msg.Cancel
+    )
+    if answer == msg.Cancel:
+        raise ModuleNotFoundError(
+            'User aborted YeastMate installation'
+        )
+
     subprocess.check_call(
         [sys.executable, '-m', 'pip', 'install',
         'git+https://github.com/hoerlteam/YeastMate']
@@ -20,7 +38,10 @@ except ModuleNotFoundError:
     # YeastMate installs opencv-python which is not functional with PyQt5 on macOS.
     # Uninstall it, and reinstall opencv-python-headless
     subprocess.check_call(
-        [sys.executable, '-m', 'pip', 'uninstall', 'opencv-python']
+        [sys.executable, '-m', 'pip', 'uninstall', '-y', 'opencv-python']
+    )
+    subprocess.check_call(
+        [sys.executable, '-m', 'pip', 'uninstall', '-y', 'opencv-python-headless']
     )
     subprocess.check_call(
         [sys.executable, '-m', 'pip', 'install', 'opencv-python-headless']
