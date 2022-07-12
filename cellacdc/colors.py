4a9a33053afcaa53c15e50747aed679291d34fd0
Francesco <padovaf@tcd.ie>
2022-07-12 10:47:50 +0200

    added new colors.py module

---
+++ cellacdc/colors.py
@@ -0,0 +1,115 @@
+import re
+import traceback
+
+from collections.abc import Callable, Sequence
+from pyqtgraph.colormap import ColorMap
+
+import numpy as np
+import matplotlib
+import colorsys
+import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
+
+__all__ = ['ColorMap']
+_mapCache = {}
+def getFromMatplotlib(name):
+    """
+    Added to pyqtgraph 0.12 copied/pasted here to allow pyqtgraph <0.12. Link:
+    https://pyqtgraph.readthedocs.io/en/latest/_modules/pyqtgraph/colormap.html#get
+    Generates a ColorMap object from a Matplotlib definition.
+    Same as ``colormap.get(name, source='matplotlib')``.
+    """
+    # inspired and informed by "mpl_cmaps_in_ImageItem.py", published by Sebastian Hoefer at
+    # https://github.com/honkomonk/pyqtgraph_sandbox/blob/master/mpl_cmaps_in_ImageItem.py
+    try:
+        import matplotlib.pyplot as plt
+    except ModuleNotFoundError:
+        return None
+    cm = None
+    col_map = plt.get_cmap(name)
+    if hasattr(col_map, '_segmentdata'): # handle LinearSegmentedColormap
+        data = col_map._segmentdata
+        if ('red' in data) and isinstance(data['red'], (Sequence, np.ndarray)):
+            positions = set() # super-set of handle positions in individual channels
+            for key in ['red','green','blue']:
+                for tup in data[key]:
+                    positions.add(tup[0])
+            col_data = np.zeros((len(positions),4 ))
+            col_data[:,-1] = sorted(positions)
+            for idx, key in enumerate(['red','green','blue']):
+                positions = np.zeros( len(data[key] ) )
+                comp_vals = np.zeros( len(data[key] ) )
+                for idx2, tup in enumerate( data[key] ):
+                    positions[idx2] = tup[0]
+                    comp_vals[idx2] = tup[1] # these are sorted in the raw data
+                col_data[:,idx] = np.interp(col_data[:,3], positions, comp_vals)
+            cm = ColorMap(pos=col_data[:,-1], color=255*col_data[:,:3]+0.5)
+        # some color maps (gnuplot in particular) are defined by RGB component functions:
+        elif ('red' in data) and isinstance(data['red'], Callable):
+            col_data = np.zeros((64, 4))
+            col_data[:,-1] = np.linspace(0., 1., 64)
+            for idx, key in enumerate(['red','green','blue']):
+                col_data[:,idx] = np.clip( data[key](col_data[:,-1]), 0, 1)
+            cm = ColorMap(pos=col_data[:,-1], color=255*col_data[:,:3]+0.5)
+    elif hasattr(col_map, 'colors'): # handle ListedColormap
+        col_data = np.array(col_map.colors)
+        cm = ColorMap(pos=np.linspace(0.0, 1.0, col_data.shape[0]),
+                      color=255*col_data[:,:3]+0.5 )
+    if cm is not None:
+        cm.name = name
+        _mapCache[name] = cm
+    return cm
+
+def lighten_color(color, amount=0.3, hex=True):
+    """
+    Lightens the given color by multiplying (1-luminosity) by the given amount.
+    Input can be matplotlib color string, hex string, or RGB tuple.
+
+    Examples:
+    >> lighten_color('g', 0.3)
+    >> lighten_color('#F034A3', 0.6)
+    >> lighten_color((.3,.55,.1), 0.5)
+    """
+
+    try:
+        c = matplotlib.colors.cnames[color]
+    except:
+        c = color
+    c = colorsys.rgb_to_hls(*matplotlib.colors.to_rgb(c))
+    lightened_c = colorsys.hls_to_rgb(c[0], 1 - amount * (1 - c[1]), c[2])
+    if hex:
+        lightened_c = tuple([int(round(v*255)) for v in lightened_c])
+        lightened_c = '#%02x%02x%02x' % lightened_c
+    return lightened_c
+
+def rgb_str_to_values(rgbString, errorRgb=(255,255,255)):
+    try:
+        r, g, b = re.findall(r'(\d+), (\d+), (\d+)', rgbString)[0]
+        r, g, b = int(r), int(g), int(b)
+    except TypeError:
+        try:
+            r, g, b = rgbString
+        except Exception as e:
+            print('======================')
+            traceback.print_exc()
+            print('======================')
+            r, g, b = errorRgb
+    return r, g, b
+
+def rgba_str_to_values(rgbaString, errorRgb=(255,255,255,255)):
+    try:
+        r, g, b, a = re.findall(r'(\d+), (\d+), (\d+), (\d+)', rgbaString)[0]
+        r, g, b, a = int(r), int(g), int(b), int(a)
+    except TypeError:
+        try:
+            r, g, b, a = rgbaString
+        except Exception as e:
+            r, g, b, a = errorRgb
+    return r, g, b, a
+
+def get_lut_from_colors(colors, name='mycmap', N=256, to_uint8=False):
+    cmap = LinearSegmentedColormap.from_list(name, colors, N=256)
+    lut = np.array([cmap(i)[:3] for i in np.linspace(0,1,256)])
+    if to_uint8:
+        lut = (lut*255).astype(np.uint8)
+    return lut
