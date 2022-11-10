ebf89e87b3f90df61138298e2cef75c04b34056c
James Roberts <68869888+Jamesr787@users.noreply.github.com>
2022-11-10 17:07:18 -0500

    Delta Segmentation Module

--- cellacdc/models/delta/acdcSegment.py
+++ cellacdc/models/delta/acdcSegment.py
@@ -15,8 +15,8 @@ import skimage.transform as trans
 from delta import utilities as utils
 from delta import config as cfg
 from delta.model import unet_seg
-from delta.data import binarizerange
 from delta.assets import download_assets
+
 
 class Model:
 
@@ -27,7 +27,7 @@ class Model:
 
         Parameters
         ----------
-        model_type : string, default is 2D
+        model_type : string
             The model name to be used for segmenting.
             2D or mothermachine
 
@@ -38,23 +38,13 @@ class Model:
 
         while True:
             try:
-                # Loads Presets for 2D images
-                if model_type != 'mothermachine':
-                    cfg.load_config(presets='2D')
-                    self.target_size = cfg.target_size_seg
+                # Loads Presets for '2D' or 'mothermachine' images
+                cfg.load_config(presets=model_type)
+                self.target_size = cfg.target_size_seg
 
-                    # Initialize Model and Load Weights with 2D presets
-                    self.model = unet_seg(input_size=self.target_size + (1,))
-                    self.model.load_weights(cfg.model_file_seg)
-
-                # Load Presets for mothermachine
-                else:
-                    cfg.load_config(presets='mothermachine')
-                    self.target_size = cfg.target_size_seg
-
-                    # Initialize Model and Load Weights with mothermachine Presets
-                    self.model = unet_seg(input_size=self.target_size + (1,))
-                    self.model.load_weights(cfg.model_file_seg)
+                # Initialize Model and Load Weights with 2D presets
+                self.model = unet_seg(input_size=self.target_size + (1,))
+                self.model.load_weights(cfg.model_file_seg)
 
                 break
 
@@ -68,7 +58,6 @@ class Model:
     def delta_preprocess(self,
                          image,
                          target_size: Tuple[int, int] = (256, 32),
-                         binarize: bool = False,
                          order: int = 1,
                          rangescale: bool = True,
                          crop: bool = False,
@@ -85,9 +74,6 @@ class Model:
         target_size : tupe of int or None, optional
             Size to reshape the image.
             The default is (256,32).
-        binarize : bool, optional
-            Use the binarizerange() function on the image.
-            The default is False.
         order : int, optional
             interpolation order (see skimage.transform.warp doc).
             0 is nearest neighbor
@@ -120,8 +106,6 @@ class Model:
             img = np.zeros((fill_shape[0], fill_shape[1]))
             img[0: i.shape[0], 0: i.shape[1]] = i
 
-        if binarize:
-            img = binarizerange(img)
         if rangescale:
             if np.ptp(img) != 0:
                 img = (img - np.min(img)) / np.ptp(img)
@@ -158,53 +142,52 @@ class Model:
                 f"""Delta only works with 2 dimensional images."""
             )
 
+        # 2D: Cut into overlapping windows
+        img = self.delta_preprocess(image=image,
+                                    target_size=self.target_size,
+                                    crop=True)
+
+        # Process image to use for delta
+        image = self.delta_preprocess(image=image,
+                                      target_size=self.target_size,
+                                      crop=cfg.crop_windows)
+
+        # Change Dimensions to 4D numpy array
+        image = np.reshape(image, (1,) + image.shape + (1,))
+
+        # mother machine: Don't crop images into windows
+        if not cfg.crop_windows:
+
+            # Predictions:
+            results = self.model.predict(image, verbose=1)[0, :, :, 0]
+
+            # Resize to the original shape
+            results = trans.resize(results, original_shape, anti_aliasing=True, order=1)
+
+            # Label the cells using prediction
+            lab = utils.label_seg(seg=results)
+
+        # For 2D images
         else:
-            # 2D: Cut into overlapping windows
-            img = self.delta_preprocess(image=image,
-                                        target_size=self.target_size,
-                                        crop=True)
+            # Create array to store predictions
+            results = np.zeros((1, img.shape[0], img.shape[1], 1))
 
-            # Process image to use for delta
-            image = self.delta_preprocess(image=image,
-                                          target_size=self.target_size,
-                                          crop=cfg.crop_windows)
+            # Crop, segment, stitch and store predictions in results
+            # Crop each frame into overlapping windows:
+            windows, loc_y, loc_x = utils.create_windows(
+                image[0, :, :, 0], target_size=self.target_size
+            )
+            windows = windows[:, :, :, np.newaxis]
 
-            # Change Dimensions to 4D numpy array
-            image = np.reshape(image, (1,) + image.shape + (1,))
+            # Predictions:
+            pred = self.model.predict(windows, verbose=1, steps=windows.shape[0])
+            # Stich prediction frames back together:
+            pred = utils.stitch_pic(pred[:, :, :, 0], loc_y, loc_x)
+            pred = pred[:, :, np.newaxis]
 
-            # mother machine: Don't crop images into windows
-            if not cfg.crop_windows:
+            results[0] = pred
 
-                # Predictions:
-                results = self.model.predict(image, verbose=1)[0, :, :, 0]
-
-                # Resize to the original shape
-                results = trans.resize(results, original_shape, anti_aliasing=True, order=1)
-
-                # Label the cells using prediction
-                lab = utils.label_seg(seg=results)
-
-            # For 2D images
-            else:
-                # Create array to store predictions
-                results = np.zeros((1, img.shape[0], img.shape[1], 1))
-
-                # Crop, segment, stitch and store predictions in results
-                # Crop each frame into overlapping windows:
-                windows, loc_y, loc_x = utils.create_windows(
-                    image[0, :, :, 0], target_size=self.target_size
-                )
-                windows = windows[:, :, :, np.newaxis]
-
-                # Predictions:
-                pred = self.model.predict(windows, verbose=1, steps=windows.shape[0])
-                # Stich prediction frames back together:
-                pred = utils.stitch_pic(pred[:, :, :, 0], loc_y, loc_x)
-                pred = pred[:, :, np.newaxis]
-
-                results[0] = pred
-
-                # Label the cells using prediction
-                lab = utils.label_seg(seg=results[0, :, :, 0])
+            # Label the cells using prediction
+            lab = utils.label_seg(seg=results[0, :, :, 0])
 
         return lab.astype(np.uint16)
