fix: changed scaling of images deepsea

cellacdc/models/DeepSea/__init__.py
@@ -1,5 +1,7 @@
 import os
 
+import numpy as np
+
 from cellacdc import myutils
 
 myutils.check_install_package('deepsea')
@@ -7,7 +9,21 @@ myutils.check_install_package('deepsea')
 import torch
 import torchvision.transforms as transforms
 
+from PIL import Image
+
 _, deepsea_models_path = myutils.get_model_path('deepsea', create_temp_dir=False)
+
+image_size = [383,512]
+image_means = [0.0]
+image_stds = [1.0]
+
+def _get_transforms(image_size, image_means, image_stds):
+    return transforms.Compose([
+        transforms.Resize(image_size),
+        transforms.Grayscale(num_output_channels=1),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=image_means, std=image_stds)
+    ])
 
 def _init_model(
         checkpoint_filename, DeepSeaClass, image_size, image_means, 
@@ -31,12 +47,7 @@ def _init_model(
     checkpoint = torch.load(checkpoint_path)
 
     # Initialize model
-    _transforms = transforms.Compose([
-        transforms.Resize(image_size),
-        transforms.Grayscale(num_output_channels=1),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=image_means, std=image_stds)
-    ])
+    _transforms = _get_transforms(image_size, image_means, image_stds)
 
     model = DeepSeaClass(
         n_channels=1, n_classes=2, bilinear=True
@@ -44,3 +55,13 @@ def _init_model(
     model.load_state_dict(checkpoint)
     model = model.to(torch_device)
     return _transforms, torch_device, checkpoint, model
+
+def _resize_img(img: Image.Image, image_size, image_means, image_stds, device):
+    _transforms = _get_transforms(image_size, image_means, image_stds)
+    tensor_img = _transforms(img).to(device=device, dtype=torch.float32)
+    resized_img = tensor_img.cpu().numpy()[0,:,:]
+    img_min = np.min(resized_img)
+    img_max = np.max(resized_img)
+    img_range = img_max - img_min
+    resized_img = (resized_img - img_min) / img_range * 255
+    return resized_img

cellacdc/models/DeepSea/acdcSegment.py
@@ -13,6 +13,7 @@ from deepsea.model import DeepSeaSegmentation
 from cellacdc import myutils, printl
 
 from . import deepsea_models_path, _init_model
+from . import image_size, image_means, image_stds
 
 SEED = 1234
 random.seed(SEED)
@@ -24,8 +25,8 @@ torch.backends.cudnn.deterministic = True
 class Model:
     def __init__(self, gpu=False):
         _transforms, torch_device, checkpoint, model = _init_model(
-            'segmentation.pth', DeepSeaSegmentation, [383,512], 
-            [0.5], [0.5]
+            'segmentation.pth', DeepSeaSegmentation, image_size, 
+            image_means, image_stds
         )
         self.torch_device = torch_device
         self._transforms = _transforms
@@ -38,29 +39,35 @@ class Model:
         labels = np.zeros(image.shape, dtype=np.uint32)
         if is_rgb_image:
             labels = np.zeros(image.shape[:-1], dtype=np.uint32)
+            
         else:
             labels = np.zeros(image.shape, dtype=np.uint32)
 
+        Y, X = labels.shape[-2:]
+
         if is_z_stack:
             for z, img in enumerate(image):
-                labels[z] = self._segment_2D_image(img)
+                labels[z] = self._segment_2D_image(img, (Y, X))
         else:
-            labels = self._segment_2D_image(image)
+            labels = self._segment_2D_image(image, (Y, X))
         return labels
         
-    def _segment_2D_image(self, image: np.ndarray):
+    def _segment_2D_image(self, image: np.ndarray, grayscale_img_shape):
         img = myutils.to_uint8(image)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        img = Image.fromarray(image)
+        img = Image.fromarray(img)
         tensor_img = (
             self._transforms(img)
             .to(device=self.torch_device, dtype=torch.float32)
         )
         _eval = self.model.eval()
         mask_pred, edge_pred = _eval(tensor_img.unsqueeze(0))
+
         mask_pred = transforms.Resize(
-            image.shape, antialias=True
+            grayscale_img_shape, antialias=True
         ).forward(mask_pred)
-        mask_pred = mask_pred.argmax(dim=1).cpu().numpy()
-        lab = skimage.measure.label(np.squeeze(mask_pred))
+        mask_pred = mask_pred.argmax(dim=1).cpu().numpy()[0, :, :]
+        mask_bool = mask_pred > 0
+        lab = skimage.measure.label(np.squeeze(mask_bool))
+        
         return lab

cellacdc/trackers/DeepSea/DeepSea_tracker.py
@@ -1,12 +1,25 @@
+import cv2
+
+import numpy as np
+
+from PIL import Image
+
 from deepsea.model import DeepSeaTracker
 from deepsea.utils import track_cells
-from cellacdc.models.DeepSea import _init_model
+
+from cellacdc import myutils, printl
+from cellacdc.models.DeepSea import _init_model, _resize_img
+from cellacdc.models.DeepSea import image_size as segm_image_size
+from cellacdc.models.DeepSea import image_means as segm_image_means
+from cellacdc.models.DeepSea import image_stds as segm_image_stds
+
+from . import image_size, image_means, image_stds
 
 class tracker:
     def __init__(self, gpu=False):
         _transforms, torch_device, checkpoint, model = _init_model(
-            'tracking.pth', DeepSeaTracker, [128,128], 
-            [0.5], [0.5]
+            'tracking.pth', DeepSeaTracker, image_size, 
+            image_means, image_stds
         )
         self.torch_device = torch_device
         self._transforms = _transforms
@@ -14,5 +27,28 @@ class tracker:
         self.model = model
     
     def track(self, segm_video, image, signals=None):
-        for img in image:
-            pass
+        labels_list = []
+        resize_img_list = []
+        for img, lab in image:
+            img = myutils.to_uint8(img)
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            img = Image.fromarray(img)
+            resized_img = _resize_img(
+                img, segm_image_size, segm_image_means, segm_image_stds,
+                self.torch_device
+            )
+            resize_lab = _resize_img(
+                lab, segm_image_size, segm_image_means, segm_image_stds,
+                self.torch_device
+            ).round().astype(np.uint32)
+            
+            resize_img_list.append(resize_lab)
+            labels_list.append(resize_lab)
+        
+        tracked_video, tracked_centroids, tracked_imgs = (
+            track_cells(
+                labels_list, resize_img_list, self.model, 
+                self.torch_device, transforms=self._transforms
+            )
+        )
+        return tracked_video