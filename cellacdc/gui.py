3c17ee6d280c923c38b747800f5683cb0f5c3b59
Francesco <padovaf@tcd.ie>
2023-11-10 12:38:48 +0100

    feat: toolbutton top-left to show segm 3D or 2D, closes #427

--- cellacdc/gui.py
+++ cellacdc/gui.py
@@ -1385,6 +1385,10 @@ class guiWin(QMainWindow):
         # fileToolBar.setIconSize(QSize(toolbarSize, toolbarSize))
         fileToolBar.setMovable(False)
 
+        self.segmNdimIndicatorAction = fileToolBar.addWidget(
+            self.segmNdimIndicator
+        )
+        self.segmNdimIndicatorAction.setVisible(False)
         fileToolBar.addAction(self.newAction)
         fileToolBar.addAction(self.openAction)
         fileToolBar.addAction(self.manageVersionsAction)
@@ -2443,6 +2447,11 @@ class guiWin(QMainWindow):
 
     def gui_createActions(self):
         # File actions
+        self.segmNdimIndicator = widgets.ToolButtonTextIcon(text='')
+        self.segmNdimIndicator.setCheckable(True)
+        self.segmNdimIndicator.setChecked(True)
+        self.segmNdimIndicator.setDisabled(True)        
+        
         self.newAction = QAction(self)
         self.newAction.setText("&New")
         self.newAction.setIcon(QIcon(":file-new.svg"))
@@ -14510,6 +14519,13 @@ class guiWin(QMainWindow):
 
         self.setWindowTitle(f'Cell-ACDC - GUI - "{posData.exp_path}"')
 
+        if self.isSegm3D:
+            self.segmNdimIndicator.setText('3D')
+        else:
+            self.segmNdimIndicator.setText('2D')
+            
+        self.segmNdimIndicatorAction.setVisible(True)
+
         self.guiTabControl.addChannels([posData.user_ch_name])
         self.showPropsDockButton.setDisabled(False)
 
@@ -20774,6 +20790,8 @@ class guiWin(QMainWindow):
         self.restoreDefaultColors()
         self.curvToolButton.setChecked(False)
 
+        self.segmNdimIndicatorAction.setVisible(False)
+        
         self.navigateToolBar.hide()
         self.ccaToolBar.hide()
         self.editToolBar.hide()
