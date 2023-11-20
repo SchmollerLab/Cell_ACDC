779199546fe8defd1aee7fab33b19832ebd3c21f
Timon Stegmaier <timonst2000@gmail.com>
2023-11-20 19:30:17 +0100

    Feat: Added pomBseen_nuclear

---
+++ cellacdc/models/pomBseen_nuclear/acdcSegment.py
@@ -0,0 +1,44 @@
+from pombseen.main import pomBsegNuc
+
+class Model:
+    def __init__(self, seg):
+        self.seg = seg
+        pass
+
+    def segment(
+            self,
+            image,
+            connectivity = 1,
+            offset = 0,
+            min_size=5,
+            max_size=100000,
+            max_nuclei = 2,
+            rel_size_max = 0.3         
+        ):
+        """Segment the input `image` and returns a labelled array with the same 
+        shape as input image (i.e., instance segmentation).
+
+        Parameters
+        ----------
+        image : (Y, X) np.ndarray
+            Input image
+        offset : float
+            Percentage of maximum pixel value, by default 0
+        min_size : int
+            Min size of nucleus, by default 0
+        max_size : int
+            Max size of nucleus, by default 100000
+        max_nuclei : int
+            Maximum number of nuclei per cell, by default 2
+        rel_size_max : float
+            Maximum size of nucleus in respect to cell, by default 0.3
+
+        Returns
+        -------
+        _type_
+            Segmented image
+        """       
+
+        segmented_img = pomBsegNuc(image, self.seg, connectivity, offset, min_size, max_size, max_nuclei, rel_size_max)
+
+        return segmented_img
