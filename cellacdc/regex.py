18145c5a71a23b7354737012a7bd7c5a7f139081
Francesco <padovaf@tcd.ie>
2023-12-15 22:29:02 +0100

    feat: added combine features calculator widget

--- cellacdc/regex.py
+++ cellacdc/regex.py
@@ -1,5 +1,7 @@
-def float_regex(allow_negative=True):
+def float_regex(allow_negative=True, left_chars=''):
     pattern = r'[-+]?[0-9]*\.?[0-9]*[eE]?[\-+]?[0-9]+'
+    if left_chars:
+        pattern = fr'{left_chars}{pattern}'
     if not allow_negative:
         pattern.replace('[-+]?', '[+]?')
     return pattern
