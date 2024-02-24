48f44f9e8463305af20db11795f24ab65b10c1f2
Francesco <padovaf@tcd.ie>
2024-02-24 18:28:42 +0100

    improvement: allow including nan in float regex

--- cellacdc/regex.py
+++ cellacdc/regex.py
@@ -1,14 +1,17 @@
-def float_regex(allow_negative=True, left_chars=''):
+def float_regex(allow_negative=True, left_chars='', include_nan=False):
     pattern = r'[-+]?[0-9]*\.?[0-9]*[eE]?[\-+]?[0-9]+'
     if left_chars:
         pattern = fr'{left_chars}{pattern}'
     if not allow_negative:
         pattern.replace('[-+]?', '[+]?')
+    if include_nan:
+        nan_pattern = r'NAN|Nan|NaN|nan'
+        pattern = fr'{nan_pattern}|{pattern}'
     return pattern
 
 if __name__ == '__main__':
     import re
-    s = '0.5, 2.5'
-    expr = fr'{float_regex()}'
+    s = '0.5, 2.5, nan, NaN'
+    expr = fr'{float_regex(include_nan=True)}'
     m = re.findall(expr, s.replace(' ', ''))
     print(m)
