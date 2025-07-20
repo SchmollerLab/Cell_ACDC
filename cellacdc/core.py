3273d94d6c422ebb2870f766f9486b4e393c3fb8
Teranis <timon@rapid-systems.de>
2025-07-20 22:14:20 +0200

    fix. change dtype in lab_replace_values if necessary

--- cellacdc/core.py
+++ cellacdc/core.py
@@ -248,8 +248,13 @@ def lab_replace_values(lab, rp, oldIDs, newIDs, in_place=True):
         if obj.label == newIDs[idx]:
             # Skip assigning ID to same ID
             continue
-
-        lab[obj.slice][obj.image] = newIDs[idx]
+        try:
+            lab[obj.slice][obj.image] = newIDs[idx]
+        except OverflowError:
+            # Value too large for current dtype, promote to larger dtype
+            larger_dtype = np.result_type(lab.dtype, newIDs[idx])
+            lab = lab.astype(larger_dtype)
+            lab[obj.slice][obj.image] = newIDs[idx]
     return lab
 
 def post_process_segm(labels, return_delIDs=False, **kwargs):
