c540a1bf4a19e76b66bcfe64878e11b57a704a97
keejkrej <ctyjackcao@outlook.com>
2026-05-24 09:54:54 +0200

    refactor: remove cellacdc/domain folder

--- cellacdc/domain/types.py
+++
@@ -1,8 +0,0 @@
-"""Shared type aliases for the domain model."""
-
-from typing import NewType
-
-FrameIndex = NewType('FrameIndex', int)
-CellID = NewType('CellID', int)
-ChannelName = NewType('ChannelName', str)
-PixelSize = NewType('PixelSize', float)
