8174929cad0cd40d656d368a2904a109b882f707
Francesco <padovaf@tcd.ie>
2026-09-11 13:25:08 +0200

    fix: remove duplicate eventFilter

--- cellacdc/gui.py
+++ cellacdc/gui.py
@@ -16499,10 +16499,12 @@ class guiWin(QMainWindow, whitelist.WhitelistGUIElements,
         
     def eventFilter(self, obj, ev):
         if not getattr(self, 'isDataLoaded', False):
-            return False
+            return super().eventFilter(obj, ev)
 
+        if ev.type() == QtScoped.QEventTypeAttribute('MouseButtonRelease'):
+            self.onMouseRelease()
+        
         widgetWithFocus = QApplication.focusWidget()
-        printl(widgetWithFocus)
         if widgetWithFocus is not None:
             widgetClassesFilteringEvent = (
                 'QLineEdit', 'QTextEdit', 'QPlainTextEdit', 'QAbstractSpinBox'
@@ -16511,9 +16513,8 @@ class guiWin(QMainWindow, whitelist.WhitelistGUIElements,
                 widgetWithFocus.inherits(cls) 
                 for cls in widgetClassesFilteringEvent
             ])
-            printl(widgetWithFocus, isFocusFilteringEvent)
             if isFocusFilteringEvent:
-                return False
+                return super().eventFilter(obj, ev)
         
         if (
                 ev.type() == QEvent.MouseButtonPress
@@ -16524,18 +16525,16 @@ class guiWin(QMainWindow, whitelist.WhitelistGUIElements,
         # handle presistant shortcuts (for multiple shportcuts)
         if ev.type() == QEvent.Type.KeyPress: 
             for name, key in self.widgetsPersistentShortcut.items():
-                printl(name, key, ev.key())
                 if not key == ev.key():
                     continue
                 action = self.widgetsWithShortcut[name]
-                printl(action)
                 if hasattr(action, 'click'):
                     action.click()
                 elif hasattr(action, 'trigger'):
                     action.trigger()
                 return True
 
-        return False
+        return super().eventFilter(obj, ev)
         
     @exception_handler
     def keyPressEvent(self, ev):        
@@ -36270,9 +36269,3 @@ class guiWin(QMainWindow, whitelist.WhitelistGUIElements,
             self.timestamp.updatePosViewRangeChanged(viewRange)
         
         self._viewRange = viewRange
-
-    def eventFilter(self, object, event):
-        if event.type() == QtScoped.QEventTypeAttribute('MouseButtonRelease'):
-            self.onMouseRelease()
-        
-        return super().eventFilter(object, event)
